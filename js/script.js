c25a0c28ab4a000bced5ebcd54e39791a6254489
Krystian Bieniek <krystekk2013@gmail.com>
2018-04-19 17:04:32 +0200

    Change in index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
+		return console.log('nie prawidłowe dane')
+	}
+	return a*h/2
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(20, 35),
+	triangle3Area = getTriangleArea(25, 39);
+
+console.log(triangle3Area);
+console.log(triangle2Area);
+console.log(triangle1Area);
